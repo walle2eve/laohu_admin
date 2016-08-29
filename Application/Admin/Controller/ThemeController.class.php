0e64158f459eb96de2cc112aac6b5e7dbf286abd
walle2eve <shmily_dhy@qq.com>
2016-08-29 21:57:41 +0800

    json导出

--- Application/Admin/Controller/ThemeController.class.php
+++ Application/Admin/Controller/ThemeController.class.php
@@ -31,4 +31,157 @@ class ThemeController extends BaseController
 		$this->assign('theme_stat',$theme_stat);
 		$this->display();
 	}
+	public function edit(){
+		$param = I('get.');
+
+		$result = D('ThemeInfo')->get_list($param);
+
+		$this->assign('page',$result['page']);
+		$this->assign('list',$result['list']);
+		$this->assign('param',$param);
+		$this->display();
+	}
+	//设置主题状态
+	public function set_status(){
+		$result = array(
+			'status' => true,
+		);
+
+		if(!IS_AJAX || !IS_POST)die('error page');
+
+		$id = I('post.key',0);
+		$id = intval($id);
+
+		$theme = D('ThemeInfo')->find($id);
+		if(empty($theme)){
+			$result['status'] = false;
+			$result['msg'] = '参数错误，无法修改状态，请刷新后重试';
+			$this->ajaxReturn($result);
+			exit();
+		}
+		$data['status'] = $theme['status'] == 1 ? -1 : 1;
+		$data['input_time'] = date('Y-m-d H:i:s');
+		$return = D('ThemeInfo')->where('id = %d',array($id))->save($data);
+
+		if($return === false){
+			$result['status'] = false;
+			$result['msg'] = '修改状态失败';
+			$this->ajaxReturn($result);
+			exit();
+		}
+		//A('Public')->clear_cache();
+		$this->ajaxReturn($result);
+	}
+	// 添加游戏主题信息
+	public function add_theme(){
+		$result = array(
+			'status' => true,
+			'msg' => '创建游戏主题成功',
+			'url' => U('Admin/Theme/edit'),
+		);
+		if(IS_AJAX && IS_POST){
+			$args = I('post.');
+			D('ThemeInfo')->startTrans();
+			if(!D('ThemeInfo')->create()){
+				$result['status'] = false;
+				$result['msg'] = D('ThemeInfo')->getError();
+				$this->ajaxReturn($result);
+				exit();
+			}
+			$args['status'] = 1;
+			$args['input_time'] = date('Y-m-d H:i:s');
+			$id = D('ThemeInfo')->add($args);
+			if(!$id){
+				D('ThemeInfo')->rollback();
+
+				$result['status'] = false;
+				$result['msg'] = '添加游戏主题失败';
+				$this->ajaxReturn($result);
+				exit();
+			}
+
+		//	A('Public')->clear_cache();
+			D('ThemeInfo')->commit();
+			$this->ajaxReturn($result);
+			exit();
+		}
+		die('error page');
+	}
+	// 编辑游戏主题配置信息
+	public function edit_conf(){
+		$page_error = '';
+		$theme_id = I('themeid');
+		$theme_id = intval($theme_id);
+		$ThemeInfoModel = D('ThemeInfo');
+
+		$result = $ThemeInfoModel->find($theme_id);
+		if(!$result){
+			$page_error = "参数错误,找不到指定的主题信息";
+		}
+
+		$theme_conf_field = $ThemeInfoModel->theme_conf_field;
+
+		if(IS_AJAX && IS_POST){
+			$return = array(
+				'status' => true,
+				'msg' => '编辑配置成功',
+				'url' => U('Admin/Theme/edit'),
+			);
+			if($page_error <> ''){
+				$return['status'] = false;
+				$return['msg'] = $page_error;
+				$this->ajaxReturn($return);
+				exit();
+			}
+			// 处理参数
+			$args = I('post.');
+			
+			$data['name'] = trim($args['themeName']);
+
+			$data['theme_info'] = serialize($args);
+			$data['input_time'] = date('Y-m-d H:i:s');
+			$re = $ThemeInfoModel->where('id = %d',array($theme_id))->save($data);
+
+			if($re){
+				$this->ajaxReturn($return);
+			}else{
+				$return['status'] = false;
+				$return['msg'] = '编辑配置信息失败，请重试！';
+				$this->ajaxReturn($return);
+			}
+			
+			exit();
+		}
+
+		$result['theme_info'] = unserialize($result['theme_info']);
+
+		$this->assign('page_error',$page_error);
+		$this->assign('result',$result);
+		$this->assign('theme_conf_field',$theme_conf_field);
+		$this->display();
+	}
+	// 导出app所需json格式
+	public function export_json(){
+		$ThemeInfoModel = D('ThemeInfo');
+		$list = $ThemeInfoModel->where('status = 1')->select();
+
+		if(empty($list))$this->error('没有可以导出的主题配置信息');
+
+		$theme_conf_field = $ThemeInfoModel->theme_conf_field;
+
+		foreach($theme_conf_field as $key=>$val){
+			$theme_conf_field_arr[$key] = $val['field_type'] == 'string' ? '' : array();
+		}
+
+		$json_data = array();
+
+		foreach($list as $row){
+			$theme_info = unserialize($row['theme_info']);
+			if(empty($theme_info))$theme_info = $theme_conf_field_arr;
+			$json_data[$row['id']] = $theme_info;
+		}
+
+		header('Content-type:text/json');
+		return $this->ajaxReturn($json_data);
+	}
 }
---
+++ Application/Admin/Model/ThemeInfoModel.class.php
@@ -0,0 +1,86 @@
+<?php
+namespace Admin\Model;
+use Think\Model;
+
+class ThemeInfoModel extends Model{
+    protected $fields = array(
+								'id',
+								'name',
+								'status',
+								'theme_info',
+								'input_time',
+							);
+
+    protected $pk     = 'id';
+
+	protected $_validate = array(
+		array('id','require','游戏主题ID必须'),
+		array('id','','游戏主题ID已经存在',0,'unique',1),
+		array('name','require','游戏主题名称必须'),
+	);
+
+	public $theme_conf_field = array(
+		'themeid' => array('field_type'=>'string'),
+		'themeName' => array('field_type'=>'string'),
+		'atlasName' => array('field_type'=>'string'),
+		'assetName' => array('field_type'=>'string'),
+		'iconName' => array('field_type'=>'string'),
+		'Logo' => array('field_type'=>'string'),
+		'backGround' => array('field_type'=>'string'),
+		'lineNum' => array('field_type'=>'string'),
+		'num' => array('field_type'=>'string'),
+		'model' => array('field_type'=>'string'),
+		'MiniLogo' => array('field_type'=>'string'),
+		'BackImage' => array('field_type'=>'string'),
+		'LoadingInfo' => array('field_type'=>'string'),
+		'folder'=> array('field_type'=>'string'),
+		'backMusic' => array('field_type'=>'string'),
+		'spinMusic' => array('field_type'=>'string'),
+		'stopMusic' => array('field_type'=>'string'),
+		'scale' => array('field_type'=>'string'),
+		'width' => array('field_type'=>'string'),
+		'height' => array('field_type'=>'string'),
+		'port' => array('field_type'=>'string'),
+		'versionCode' => array('field_type' => 'string'),
+		'AnimationList' => array('field_type'=>'list','field_infos'=>array(
+			'SpriteName',
+			'id',
+			'AnimationName',
+			'ImageCount',
+			'framesPerSecond',
+			'width',
+			'height',
+			'hasZero',
+			)),
+		'SoundList' => array('field_type' => 'list','field_infos' => array(
+			'key',
+			'value',
+			)),
+		'file' => array('field_type' => 'list', 'field_infos' => array(
+			'FileName',
+			'Version',
+			'DateTime',
+			)),
+		);
+
+	public function get_list($param = array(),$status = 0){
+
+		$where = ' 1=1 ';
+		
+		if($status <> 0) $where .= " AND status =  " . intval($status);
+
+		if(isset($param['keyword']) && trim($param['keyword']) != ''){
+			$where .= " AND (name LIKE '%".$param['keyword']."%') ";
+		}
+
+		$count = $this->where($where)->count();
+
+		$page = page($count);
+
+		$list = $this->where($where)
+					//->order('uid DESC')
+					->limit($page->firstRow.','.$page->listRows)
+					->select();
+		return array('list'=>$list,'page'=>$page->show());
+	}
+}
